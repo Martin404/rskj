0ae24544006cf30a2f535a37a4deebb35252ba1b
Joaquin Diaz Trepat <joaquin@rsk.co>
2018-02-16 17:47:43 -0300

    On any other call, doCALL

--- rskj-core/src/main/java/org/ethereum/vm/VM.java
+++ rskj-core/src/main/java/org/ethereum/vm/VM.java
@@ -1852,6 +1852,8 @@ public class VM {
             case OpCodes.OP_CALL:
             case OpCodes.OP_CALLCODE:
             case OpCodes.OP_DELEGATECALL:
+                doCALL();
+            break;
             case OpCodes.OP_STATICCALL:
                 if (!config.isRcs230()) {
                     throw Program.ExceptionHelper.invalidOpCode(program.getCurrentOp());
